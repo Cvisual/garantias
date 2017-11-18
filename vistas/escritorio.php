89078fa17eeb06fd7166357d75c9420c06a47c3a
studio34k <studioaudiovisual34k@gmail.com>
2017-11-18 00:36:15 -0500

    Se modifican los links del escritorios

--- vistas/escritorio.php
+++ vistas/escritorio.php
@@ -42,14 +42,14 @@ require 'header.php';
                                 <h4 style="font-size:17px;">
                                   <!--<strong>S/ <?php echo $totalv; ?></strong>-->
                                 </h4>
-                                <a class="link" href="serviciotecnico.php">
+                                <a class="link" href="serviciot.php">
                                   <p id="titulo">Servicio Técnico</p><i class="fa fa-cogs fa-5x iconos" aria-hidden="true"></i>
                                 </a>
                               </div>
                               <div class="icon">
                                 <i class="ion ion-bag"></i>
                               </div>
-                              <a href="serviciotecnico.php" class="small-box-footer">Servicio Tecnico  <i class="fa fa-arrow-circle-right"></i></a>
+                              <a href="serviciot.php" class="small-box-footer">Servicio Tecnico  <i class="fa fa-arrow-circle-right"></i></a>
                             </div>
                         </div>
                         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
@@ -58,7 +58,7 @@ require 'header.php';
                                 <h4 style="font-size:17px;">
                                   <!--<strong>S/ <?php echo $totalv; ?></strong>-->
                                 </h4>
-                                <a class="link" href="clientefinal.php">
+                                <a class="link" href="clientef.php">
                                   <p id="titulo">Cliente Final</p>
                                 <i class="fa fa-user fa-5x iconos" aria-hidden="true"></i>
                                 </a>
@@ -66,7 +66,7 @@ require 'header.php';
                               <div class="icon">
                                 <i class="ion ion-bag"></i>
                               </div>
-                              <a href="clientefinal.php" class="small-box-footer">Cliente Final  <i class="fa fa-arrow-circle-right"></i></a>
+                              <a href="clientef.php" class="small-box-footer">Cliente Final  <i class="fa fa-arrow-circle-right"></i></a>
                             </div>
                         </div>
                     </div>
